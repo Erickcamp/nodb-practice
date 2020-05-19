e0f8d8cf4368cad3d232a056d9f66a5a2bfdbf39
Erickcamp <thecampkid@gmail.com>
2020-05-18 22:35:23 -0600

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component  {
+  constructor(){
+    super()
+    this.state = {
+      todos: ['Dishes', 'Garbage', 'Learn to code', 'Destroy the Death Star']
+    }
+  }
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.todos.map(todo => {
+        return (<p>{todo}</p>)
+      })}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
